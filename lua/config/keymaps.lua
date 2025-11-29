d6774de14d31dac23a7039c7a704a63dd6dc9f8f
minij147 <jakepaul628@gmail.com>
2025-11-28 19:20:15 -0500

    updated for window and tabs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,10 +17,19 @@ keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
 keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontally
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- Toggle Minimise
 
+keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
+keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
+keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
+keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
+
 -- Temrinal Window
 keymap.set("n", "<leader>tc", ":term <CR> a", opts)
 keymap.set("n", "<leader>tv", ":vertical terminal <CR> a", opts) -- (a) inserts automatically
 keymap.set("n", "<leader>th", ":horizontal terminal <CR> a", opts)
+
+-- Tab Management
+keymap.set("n", "<leader>tn", ":tabn <CR>") -- move to next tab
+keymap.set("n", "<leader>T", ":tabnew <CR>") -- new tab
 
 -- normal view out of terminal
 keymap.set("t", "<C-\\>", "<C-\\><C-n>", opts) -- lets us leave terminal mode with Crtl-Space
